44f8de5dfff9193008bd6ff3a0829b5faa513c53
Brendan Stanton <bmsenate@gmail.com>
2018-01-22 14:56:01 -0800

    Initial Commit

---
+++ umamiStar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  umamiStar
+//
+//  Created by Brendan Stanton on 1/22/18.
+//  Copyright © 2018 Brendan Stanton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
